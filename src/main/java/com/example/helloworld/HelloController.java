c6c66c277448b0dba09827c0d82e1b3870affe0d
kamide28 <ghmaho0928@gmail.com>
2023-04-06 22:09:09 +0900

    HelloWorldに現在時刻表示を追加

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -3,11 +3,17 @@ package com.example.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!!";
+        LocalDateTime nowTime = LocalDateTime.now();
+        DateTimeFormatter formattingTime = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mmです");
+        String formattedTime = formattingTime.format(nowTime);
+        return "Hello World!! 現在時刻（日本）は" + formattedTime;
     }
 }
